f1e8f4c531c02112db4439363fc2aa1f26c4432e
Ali <148422992+KingMan0406@users.noreply.github.com>
2024-06-19 07:21:37 +0600

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    phone: String,
-    passport: String,
-    departureCity: String,
-    destinationCity: String,
-    date: Date,
-    flightId: { type: mongoose.Schema.Types.ObjectId, ref: 'Flight' },
-    amount: Number,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phone: { type: String, required: true },
+    passport: { type: String, required: true },
+    departureCity: { type: String, required: true },
+    destinationCity: { type: String, required: true },
+    date: { type: Date, required: true },
+    flightId: { type: mongoose.Schema.Types.ObjectId, ref: 'Flight', required: true },
+    amount: { type: Number, required: true },
     status: { type: String, default: 'booked' },
 });
 
 module.exports = mongoose.model('Booking', BookingSchema);
+
