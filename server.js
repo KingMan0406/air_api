dcc60c3368d8bf5032edd9fbe972cadcdf157767
Ali <148422992+KingMan0406@users.noreply.github.com>
2024-06-19 07:31:35 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const flightRoutes = require('./routes/api');  // Предполагается, что ваш маршрутизатор находится в api.js
+const flightRoutes = require('./routes/api');
 
 const app = express();
 
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const mongoURI = process.env.DATABASE_URL || 'mongodb+srv://kingman0406:inai2203114@cluster0.cnonrgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.DATABASE_URL || 'mongodb+srv://kingman0406:<inai2203114>@cluster0.cnonrgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
