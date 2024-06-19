ff64ac8378773b91094f8dc0a14b89fa0d98f50f
Ali <148422992+KingMan0406@users.noreply.github.com>
2024-06-19 07:22:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const flightRoutes = require('./routes/api');  // Предполагается, что ваш маршрутизатор находится в api.js
 
 const app = express();
 
@@ -10,18 +11,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const mongoURI = process.env.DATABASE_URL || 'mongodb+srv://kingman0406:<inai2203114>@cluster0.cnonrgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.DATABASE_URL || 'mongodb+srv://kingman0406:inai2203114@cluster0.cnonrgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
-app.use('/api', require('./routes/api'));
+app.use('/api', flightRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
