198212a7240d2e692f20f160202343d4d108226d
Ali <148422992+KingMan0406@users.noreply.github.com>
2024-06-19 07:21:55 +0600

    Update Flight.js

--- models/Flight.js
+++ models/Flight.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const FlightSchema = new mongoose.Schema({
-    flightNumber: String,
-    departureCity: String,
-    destinationCity: String,
-    departureTime: Date,
-    arrivalTime: Date,
-    price: Number,
+    flightNumber: { type: String, required: true },
+    departureCity: { type: String, required: true },
+    destinationCity: { type: String, required: true },
+    departureTime: { type: Date, required: true },
+    arrivalTime: { type: Date, required: true },
+    price: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Flight', FlightSchema);
+
